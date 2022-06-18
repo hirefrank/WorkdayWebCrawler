656af39c910e1ac2f3cdfdd3e4aaffc013f6c90c
elhardoum <elhardoum@outlook.com>
2022-06-18 10:28:51 -0600

    deno scraper with workers for threading

--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,20 @@
 import { Command } from 'https://deno.land/x/cliffy@v0.24.2/command/mod.ts'
+import { join } from 'https://deno.land/std@0.144.0/path/mod.ts'
 import { getJobPostings } from './crawler.ts'
 
 export interface CliArguments {
-  url: string,
-  dest: string,
-  threads: number,
-  verbose?: boolean,
+  url: string
+  dest: string
+  threads: number
+  verbose?: boolean
 }
 
-export default () => new Command()
+export default ( baseDir: string ) => new Command()
   .name('Workday web crawler')
   .version('0.1')
   .description('Workday web crawler CLI')
   .option('-u, --url <url:string>', 'Job Posting URL', {
-    default: 'https://mastercard.wd1.myworkdayjobs.com/CorporateCareers' as const,
+    default: 'https://mastercard.wd1.myworkdayjobs.com/wday/cxs/mastercard/CorporateCareers/jobs' as const,
     required: true,
   })
   .option('-d, --dest <dest:string>', 'Destination Directory', {
@@ -32,6 +33,6 @@ export default () => new Command()
     if ( threads <= 0 )
       throw Error('threads value cannot be less than/equal to zero')
 
-    getJobPostings({ url, dest, threads, verbose })
+    getJobPostings({ url, dest: join(baseDir, dest), threads, verbose })
   })
   .parse(Deno.args)
